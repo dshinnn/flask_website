60c2207c45f7e6b31b6fc156115b7258b2c16cf8
David Shin <shin.david93@gmail.com>
2022-01-19 07:17:42 -0800

    Add a placeholder for front page and add redirect after registration

--- app/routes.py
+++ app/routes.py
@@ -12,4 +12,8 @@ def phonebook():
     
     if form.validate_on_submit():
         print(form.name.data, form.phonenumber.data, form.email.data, form.address.data)
+        with open('contacts.txt', 'a') as file:
+            file.write(f'\nName: {form.name.data},  Phone Number: {form.phonenumber.data}, Email: {form.email.data}, Address: {form.address.data}')
+        return redirect(url_for('index'))
+
     return render_template('phonebook.html', form=form)
