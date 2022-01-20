23acda5d70b7c51ca085d2633cecf745971aeeac
David Shin <shin.david93@gmail.com>
2022-01-19 21:05:06 -0800

    Add register, login, logout and require user to be logged in to add to phonebook

--- app/form.py
+++ app/form.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class PhonebookForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     phonenumber = StringField('Phone Number', validators=[DataRequired()])
-    email = StringField('Email', validators=[Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Add Contact')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,73 @@
+import json
 from app import app
 from flask import render_template, url_for, redirect
-from app.form import PhonebookForm
+from flask_login import login_user, logout_user, login_required
+from app.form import PhonebookForm, RegisterForm, LoginForm
+from app.models import User, Phonebook
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/phonebook', methods=['GET', 'POST'])
+@login_required
 def phonebook():
     form = PhonebookForm()
     
     if form.validate_on_submit():
-        print(form.name.data, form.phonenumber.data, form.email.data, form.address.data)
-        with open('contacts.txt', 'a') as file:
-            file.write(f'Name: {form.name.data},  Phone Number: {form.phonenumber.data}, Email: {form.email.data}, Address: {form.address.data}\n')
+        name = form.name.data
+        phonenumber = form.phonenumber.data
+        email = form.email.data
+        address = form.address.data
+
+        Phonebook(name=name, phonenumber=phonenumber, email=email, address=address)
         return redirect(url_for('index'))
 
     return render_template('phonebook.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+        
+        if not user:
+            User(username=username, password=password)
+            return render_template('index.html')
+        else:
+            print('Username already exists')
+        return render_template('register.html')
+        
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    # Checks if username and password are not null
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        # Checks if the username exists and if the password matches
+        user = User.query.filter_by(username=username).first()
+        if not user or not user.check_password(password):
+            print('Username does not exist')
+            return redirect(url_for('login'))
+
+        # If username exists and the password match, login user
+        login_user(user)
+        print('Sucessfully logged in')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    print('Successfully logged out')
+    return redirect(url_for('index'))
